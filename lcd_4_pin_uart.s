fc9eb0eead684312762c89b15f9a1105a51aa435
napicell <napicell@amazon.com>
2024-04-10 19:00:04 +0200

    Working implementation of uart with lcd

--- lcd_4_pin_uart.s
+++ lcd_4_pin_uart.s
@@ -1,25 +1,12 @@
-; issue when sending lower case characters from MCU to serial/
-; Upper case characters are sent correctly but lower case are not.
-;
-; 01101000 h becomes
-; 01111000 x
-
-
-; 01101101 m becomes
-; 01111101 }
-
-; expected string
-; abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ
-; becomes
-; abcdefgxyz{|}~pqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ
-
-
 PORTB = $8000
 DDRB = $8002
 
-UART_READ = %1001000000000010
-UART_WRITE = %1001000000000001
-UART_DATA_AVAILABLE = %1001000000000100;$9003
+UART_WRITE  = %1001000000000001
+UART_READ   = %1001000000000010
+UART_STATUS = %1001000000000100
+
+UART_WRITE_BUSY_MASK   = %00000001 
+UART_READ_NO_DATA_MASK = %00000010
 
 E  = %01000000
 RW = %00100000
@@ -55,32 +42,12 @@ init:
   ldx #$00
 
 
-; main:
-;   lda #%00000010
-;   and UART_DATA_AVAILABLE
-;   bne main ; can't transfer yet cause TXE is low
-
-;   lda message,x
-;   sta UART_WRITE
-
-;   jmp loop
-
-
 wait_input:
-  lda #%00000001
-  and UART_DATA_AVAILABLE
+  lda #UART_READ_NO_DATA_MASK
+  and UART_STATUS
   bne wait_input   ; no data available
 
   jmp read_uart
-
-  ; ldx #$00 
-  ; lda message,x
-  ; jsr print_char
-
-  ; ldx #$00
-  ; lda message,x
-  ; sta UART_WRITE
-
   ldx #$00
   jmp print_uart_loop
   jmp loop
@@ -93,8 +60,8 @@ read_uart:
   lda UART_READ
   jsr print_char
 
-  lda #%00000001
-  and UART_DATA_AVAILABLE
+  lda #UART_READ_NO_DATA_MASK
+  and UART_STATUS
   bne ack   ; no data available
   jmp read_uart
 
@@ -102,18 +69,13 @@ read_uart:
 loop:
   jmp loop
 
-; print_uart_exit:
-;   rts
-
 
 print_uart:
   ldx #$00
 print_uart_loop:
-  ; lda #%00000010
-  ; and UART_DATA_AVAILABLE
-  ; bne print_uart_loop ; can't transfer yet cause TXE is low
-
-  ;jsr busy_wait
+  lda #UART_WRITE_BUSY_MASK
+  and UART_STATUS
+  bne print_uart_loop ; can't transfer yet cause TXE is high
 
   lda message,x
   beq wait_input
@@ -122,10 +84,9 @@ print_uart_loop:
   
   jmp print_uart_loop
 
-;message: .byte "my string", 0
+
 message: .asciiz "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-;message: .asciiz "hijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-;message: .asciiz "abdce"
+;message: .byte "my string", 0
 ;  message: .byte %00000001
 ; .byte %00000010
 ; .byte %00000100
@@ -216,268 +177,4 @@ print_char:
   sta PORTB
   eor #E          ; Clear E bit
   sta PORTB
-  rts
-
-busy_wait:
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-  nop
-
   rts
